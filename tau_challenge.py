556fb00365040e812a95c609ffe24842d3c213fb
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-22 00:11:08 -0500

    3.23 LAB: Challenge program (optional)

---
+++ tau_challenge.py
@@ -0,0 +1,14 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: SHRUTIKA SURESH
+# Section: 406/506
+# Assignment: Lab 3 - Idividual
+# Date: 19/09/2025
+#
+import math
+x = int(input("Please enter the number of digits of precision for tau: "))
+tou = math.tau
+print (f'The value of tau to {x} digits is: {tou:.{x}f}')
+# that wasn't even hard lol
