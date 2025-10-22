a2b68a8d2f3c2e8e3f96e7a7bd79e1fe16458d99
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-22 00:13:22 -0500

    4.19 LAB: Largest number

---
+++ largest_number.py
@@ -0,0 +1,20 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: SHRUTIKA SURESH
+# Section: 406/506
+# Assignment: Lab 4 - Idividual
+# Date:23/09/2025
+#
+#
+num1 = float(input('Enter number 1: '))
+num2 = float(input('Enter number 2: '))
+num3 = float(input('Enter number 3: '))
+max = num1
+if(max<num2):
+    max=num2
+if(max<num3):
+    max=num3
+#basically just  two if statements abt everything comment comment comment
+print('The largest number is', max)
