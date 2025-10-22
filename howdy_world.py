93b1c05a626d1edc80e393f9566a97bef09d06b3
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-21 23:42:07 -0500

    1.11 LAB: Howdy World

---
+++ howdy_world.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: SHRUTIKA SURESH
+# Section: YOUR SECTION NUMBER
+# Assignment: Lab Topic 1
+# Date: 29 08 2025
+#
+#
+print ("Howdy, World!")
+print ("My favorite color is red.")
