52448dd9ee87f0a5fe8b46ceeb38c0a1b23510dd
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-22 00:16:33 -0500

    5.4 LAB: Ten Year Risk
    i hated this FUCK ASS LAB omfg kill me now
    WHAT WAS THE POINT OF THIS

---
+++ tyr_test_cases.py
@@ -0,0 +1,203 @@
+##FEMALE            100 %
+
+sex:F age:20 cho:100 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:F age:20 cho:160 smo:N hdl:50 sbp:119 med:Y out:<1
+sex:F age:24 cho:200 smo:Y hdl:59 sbp:120 med:N out:1
+sex:F age:39 cho:240 smo:Y hdl:40 sbp:120 med:Y out:14
+sex:F age:39 cho:280 smo:N hdl:49 sbp:130 med:N out:2
+
+sex:F age:40 cho:100 smo:Y hdl:39 sbp:130 med:Y out:2
+sex:F age:40 cho:160 smo:N hdl:60 sbp:140 med:N out:<1
+sex:F age:44 cho:200 smo:Y hdl:50 sbp:140 med:Y out:6
+sex:F age:49 cho:240 smo:Y hdl:59 sbp:160 med:N out:17
+sex:F age:49 cho:280 smo:N hdl:40 sbp:160 med:Y out:11
+
+sex:F age:50 cho:100 smo:Y hdl:49 sbp:129 med:N out:1
+sex:F age:50 cho:160 smo:N hdl:39 sbp:129 med:Y out:2
+sex:F age:54 cho:200 smo:Y hdl:60 sbp:139 med:N out:3
+sex:F age:59 cho:240 smo:Y hdl:50 sbp:139 med:Y out:14
+sex:F age:59 cho:280 smo:N hdl:59 sbp:159 med:N out:6
+
+sex:F age:60 cho:100 smo:Y hdl:40 sbp:159 med:Y out:6
+sex:F age:60 cho:160 smo:N hdl:49 sbp:161 med:N out:4
+sex:F age:64 cho:200 smo:Y hdl:39 sbp:161 med:Y out:17
+sex:F age:69 cho:240 smo:Y hdl:60 sbp:100 med:N out:4
+sex:F age:69 cho:280 smo:N hdl:50 sbp:100 med:N out:4
+
+sex:F age:70 cho:100 smo:Y hdl:59 sbp:120 med:N out:4
+sex:F age:70 cho:160 smo:N hdl:40 sbp:130 med:N out:6
+sex:F age:74 cho:200 smo:Y hdl:49 sbp:140 med:N out:11
+sex:F age:79 cho:240 smo:Y hdl:39 sbp:160 med:N out:>30
+sex:F age:79 cho:280 smo:N hdl:60 sbp:139 med:N out:8
+
+sex:F age:2 cho:100 smo:Y hdl:50 sbp:119 med:N out:<1
+sex:F age:2 cho:100 smo:N hdl:59 sbp:119 med:N out:<1
+sex:F age:80 cho:290 smo:Y hdl:40 sbp:119 med:N out:11
+sex:F age:80 cho:290 smo:N hdl:49 sbp:119 med:N out:8
+
+
+#MALE
+sex:M age:20 cho:100 smo:Y hdl:60 sbp:119 med:N out:<1
+sex:M age:20 cho:160 smo:N hdl:50 sbp:119 med:Y out:<1
+sex:M age:24 cho:200 smo:Y hdl:59 sbp:120 med:N out:2
+sex:M age:39 cho:240 smo:Y hdl:40 sbp:120 med:Y out:20
+sex:M age:39 cho:280 smo:N hdl:49 sbp:130 med:N out:5
+
+sex:M age:40 cho:100 smo:Y hdl:39 sbp:130 med:Y out:5
+sex:M age:40 cho:160 smo:N hdl:60 sbp:140 med:N out:1
+sex:M age:44 cho:200 smo:Y hdl:50 sbp:140 med:Y out:10
+sex:M age:49 cho:240 smo:Y hdl:59 sbp:160 med:N out:25
+sex:M age:49 cho:280 smo:N hdl:40 sbp:160 med:Y out:20
+
+
+sex:M age:50 cho:100 smo:Y hdl:49 sbp:129 med:N out:6
+sex:M age:50 cho:160 smo:N hdl:39 sbp:129 med:Y out:8
+sex:M age:54 cho:200 smo:Y hdl:60 sbp:139 med:N out:10
+sex:M age:59 cho:240 smo:Y hdl:50 sbp:139 med:Y out:>30
+sex:M age:59 cho:280 smo:N hdl:59 sbp:159 med:N out:16
+
+sex:M age:60 cho:100 smo:Y hdl:40 sbp:159 med:Y out:16
+sex:M age:60 cho:160 smo:N hdl:49 sbp:161 med:N out:16
+sex:M age:64 cho:200 smo:Y hdl:39 sbp:161 med:Y out:>30
+sex:M age:69 cho:240 smo:Y hdl:60 sbp:100 med:N out:12
+sex:M age:69 cho:280 smo:N hdl:60 sbp:100 med:N out:12
+
+sex:M age:70 cho:100 smo:Y hdl:50 sbp:140 med:Y out:20
+sex:M age:70 cho:160 smo:N hdl:59 sbp:129 med:Y out:12
+sex:M age:74 cho:200 smo:Y hdl:40 sbp:130 med:Y out:25
+sex:M age:79 cho:240 smo:Y hdl:49 sbp:159 med:Y out:>30
+sex:M age:79 cho:280 smo:N hdl:39 sbp:161 med:Y out:>30
+
+sex:M age:2 cho:240 smo:Y hdl:60 sbp:0 med:Y out:3
+sex:M age:2 cho:240 smo:N hdl:50 sbp:0 med:N out:1
+sex:M age:80 cho:280 smo:Y hdl:59 sbp:-20 med:N out:20
+sex:M age:80 cho:280 smo:N hdl:40 sbp:-20 med:N out:20
+
+
+##miscellaneous
+sex:F age:0 cho:0 smo:N hdl:0 sbp:0 med:N out:<1
+sex:M age:0 cho:0 smo:N hdl:0 sbp:0 med:N out:<1
+sex:F age:100 cho:400 smo:Y hdl:100 sbp:200 med:Y out:27
+sex:M age:100 cho:400 smo:Y hdl:100 sbp:200 med:Y out:>30
+
+
+sex:F age:30 cho:150 smo:Y hdl:30 sbp:110 med:N out:<1
+sex:M age:30 cho:150 smo:Y hdl:30 sbp:110 med:N out:1
+sex:M age:70 cho:250 smo:N hdl:70 sbp:150 med:Y out:16
+sex:F age:70 cho:250 smo:N hdl:70 sbp:150 med:Y out:11
+
+
+sex:F age:25 cho:180 smo:N hdl:55 sbp:115 med:Y out:<1
+sex:M age:25 cho:180 smo:N hdl:55 sbp:115 med:Y out:<1
+sex:M age:55 cho:220 smo:Y hdl:45 sbp:135 med:N out:25
+sex:F age:55 cho:220 smo:Y hdl:45 sbp:135 med:N out:8
+
+
+sex:M age:-1 cho:-1 smo:N hdl:-1 sbp:-1 med:N out:<1
+sex:F age:-1 cho:-1 smo:Y hdl:-1 sbp:-1 med:Y out:<1
+
+
+sex:F age:45 cho:300 smo:Y hdl:20 sbp:180 med:Y out:>30
+sex:M age:45 cho:300 smo:Y hdl:20 sbp:180 med:Y out:>30
+sex:M age:35 cho:120 smo:N hdl:80 sbp:90 med:N out:<1
+sex:F age:35 cho:120 smo:N hdl:80 sbp:90 med:N out:<1
+
+
+sex:F age:1 cho:100 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:1 cho:100 smo:Y hdl:60 sbp:100 med:N out:<1
+sex:M age:99 cho:300 smo:Y hdl:40 sbp:180 med:Y out:>30
+sex:F age:99 cho:300 smo:Y hdl:40 sbp:180 med:Y out:>30
+
+
+sex:F age:50 cho:50 smo:N hdl:60 sbp:120 med:N out:<1
+sex:M age:50 cho:50 smo:N hdl:60 sbp:120 med:N out:2
+sex:M age:50 cho:350 smo:Y hdl:60 sbp:120 med:Y out:16
+sex:F age:50 cho:350 smo:Y hdl:60 sbp:120 med:Y out:8
+
+
+sex:F age:50 cho:200 smo:N hdl:20 sbp:120 med:N out:2
+sex:M age:50 cho:200 smo:N hdl:20 sbp:120 med:N out:8
+sex:M age:50 cho:200 smo:Y hdl:100 sbp:120 med:Y out:10
+sex:F age:50 cho:200 smo:Y hdl:100 sbp:120 med:Y out:4
+
+sex:F age:60 cho:300 smo:N hdl:20 sbp:120 med:N out:5
+sex:M age:60 cho:300 smo:N hdl:20 sbp:120 med:N out:20
+sex:M age:60 cho:300 smo:Y hdl:100 sbp:120 med:Y out:16
+sex:F age:60 cho:300 smo:Y hdl:100 sbp:120 med:Y out:6
+
+sex:F age:50 cho:200 smo:N hdl:60 sbp:80 med:N out:1
+sex:M age:50 cho:200 smo:N hdl:60 sbp:80 med:N out:4
+sex:M age:50 cho:200 smo:Y hdl:60 sbp:200 med:Y out:16
+sex:F age:50 cho:200 smo:Y hdl:60 sbp:200 med:Y out:8
+
+
+sex:F age:25 cho:300 smo:Y hdl:20 sbp:180 med:Y out:22
+sex:M age:25 cho:300 smo:Y hdl:20 sbp:180 med:Y out:20
+sex:M age:75 cho:100 smo:N hdl:100 sbp:90 med:N out:10
+sex:F age:75 cho:100 smo:N hdl:100 sbp:90 med:N out:3
+
+sex:F age:99 cho:0 smo:Y hdl:20 sbp:180 med:Y out:>30
+sex:M age:99 cho:199 smo:Y hdl:20 sbp:180 med:Y out:>30
+sex:M age:99 cho:200 smo:N hdl:100 sbp:90 med:N out:10
+sex:F age:99 cho:250 smo:N hdl:100 sbp:90 med:N out:5
+sex:M age:21 cho:190 smo:N hdl:38 sbp:118 med:N out:<1
+sex:M age:27 cho:245 smo:Y hdl:55 sbp:132 med:N out:5
+sex:M age:34 cho:160 smo:N hdl:62 sbp:125 med:Y out:<1
+sex:M age:39 cho:220 smo:Y hdl:44 sbp:140 med:Y out:16
+sex:M age:46 cho:200 smo:N hdl:36 sbp:135 med:N out:8
+sex:M age:52 cho:265 smo:Y hdl:48 sbp:150 med:Y out:25
+sex:M age:58 cho:230 smo:N hdl:59 sbp:160 med:N out:12
+sex:M age:65 cho:280 smo:Y hdl:41 sbp:170 med:Y out:>30
+sex:M age:71 cho:210 smo:N hdl:50 sbp:145 med:Y out:16
+sex:M age:78 cho:195 smo:Y hdl:37 sbp:180 med:N out:>30
+
+sex:F age:22 cho:175 smo:N hdl:63 sbp:110 med:N out:<1
+sex:F age:28 cho:250 smo:Y hdl:52 sbp:120 med:N out:2
+sex:F age:33 cho:185 smo:N hdl:46 sbp:128 med:Y out:<1
+sex:F age:41 cho:215 smo:Y hdl:39 sbp:138 med:N out:5
+sex:F age:47 cho:270 smo:Y hdl:43 sbp:142 med:Y out:27
+sex:F age:53 cho:195 smo:N hdl:60 sbp:155 med:N out:1
+sex:F age:60 cho:225 smo:Y hdl:47 sbp:165 med:Y out:14
+sex:F age:67 cho:240 smo:N hdl:58 sbp:175 med:N out:8
+sex:F age:73 cho:260 smo:Y hdl:42 sbp:160 med:Y out:27
+sex:F age:79 cho:200 smo:N hdl:35 sbp:185 med:N out:22
+
+#OTHER
+
+# Ages across decades (female/male), wide cholesterol, med variations
+sex:F age:10 cho:150 smo:N hdl:60 sbp:100 med:N out:<1
+sex:M age:10 cho:150 smo:Y hdl:40 sbp:100 med:Y out:1
+sex:F age:30 cho:220 smo:Y hdl:35 sbp:135 med:N out:2
+sex:M age:30 cho:220 smo:N hdl:65 sbp:135 med:Y out:<1
+sex:F age:45 cho:260 smo:Y hdl:45 sbp:150 med:Y out:27
+sex:M age:45 cho:260 smo:Y hdl:45 sbp:150 med:N out:25
+sex:F age:55 cho:310 smo:N hdl:55 sbp:160 med:N out:8
+sex:M age:55 cho:310 smo:Y hdl:30 sbp:160 med:Y out:>30
+sex:F age:65 cho:180 smo:Y hdl:70 sbp:140 med:N out:5
+sex:M age:65 cho:180 smo:N hdl:80 sbp:140 med:N out:10
+sex:F age:85 cho:350 smo:Y hdl:25 sbp:190 med:Y out:>30
+sex:M age:85 cho:350 smo:N hdl:50 sbp:190 med:Y out:>30
+
+# HDL extremes
+sex:F age:50 cho:200 smo:Y hdl:20 sbp:120 med:N out:5
+sex:M age:50 cho:200 smo:N hdl:100 sbp:120 med:Y out:5
+sex:F age:70 cho:300 smo:Y hdl:25 sbp:170 med:Y out:>30
+sex:M age:70 cho:300 smo:Y hdl:95 sbp:170 med:N out:20
+
+# SBP extremes
+sex:F age:60 cho:240 smo:N hdl:60 sbp:80 med:N out:1
+sex:M age:60 cho:240 smo:Y hdl:60 sbp:200 med:Y out:20
+sex:F age:75 cho:180 smo:N hdl:70 sbp:200 med:N out:11
+sex:M age:75 cho:180 smo:Y hdl:40 sbp:80 med:N out:20
+
+# Cholesterol extremes
+sex:F age:40 cho:100 smo:N hdl:60 sbp:130 med:N out:<1
+sex:M age:40 cho:400 smo:Y hdl:30 sbp:150 med:Y out:>30
+sex:F age:90 cho:350 smo:Y hdl:55 sbp:160 med:Y out:>30
+sex:M age:90 cho:90 smo:N hdl:60 sbp:120 med:N out:10
+
+# Edge / nonsense cases
+sex:F age:-5 cho:200 smo:Y hdl:50 sbp:120 med:N out:1
+sex:M age:200 cho:500 smo:Y hdl:10 sbp:250 med:Y out:>30
+sex:F age:0 cho:0 smo:N hdl:0 sbp:0 med:N out:<1
+sex:M age:120 cho:450 smo:N hdl:90 sbp:180 med:N out:20
