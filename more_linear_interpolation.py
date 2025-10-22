ccf351a0e50744d0f2086ebf7aaac1f094b5857b
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-21 23:50:02 -0500

    2.10 LAB: More Linear Interpolation

---
+++ more_linear_interpolation.py
@@ -0,0 +1,47 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: SHRUTIKA SURESH
+# Section: 406/506
+# Assignment: Lab 2 - More Linear Interpolation
+# Date: 11/09/2025
+#
+#
+t1 = 12
+t2 = 85
+x1 = 8
+x2 = -5
+y1 = 6
+y2 = 30
+z1 = 7
+z2 = 9
+t = 30.0
+x = ((x2-x1)/(t2-t1))*(t - 12) + 8
+y = ((y2-y1)/(t2-t1))*(t - 12) + 6
+z = ((z2-z1)/(t2-t1))*(t - 12) + 7
+# this is all what I already know
+# after this i have NO clue
+# i'll figure it out after I deal with club apps and not stressed out of my da mn mind
+print("At time",t,"seconds:\nx1 =",x,"m\ny1 =",y,"m\nz1 =",z,"m\n-----------------------")
+t = 37.5
+x = ((x2-x1)/(t2-t1))*(t - 12) + 8
+y = ((y2-y1)/(t2-t1))*(t - 12) + 6
+z = ((z2-z1)/(t2-t1))*(t - 12) + 7
+print("At time",t,"seconds:\nx2 =",x,"m\ny2 =",y,"m\nz2 =",z,"m\n-----------------------")
+t = 45.0
+x = ((x2-x1)/(t2-t1))*(t - 12) + 8
+y = ((y2-y1)/(t2-t1))*(t - 12) + 6
+z = ((z2-z1)/(t2-t1))*(t - 12) + 7
+print("At time",t,"seconds:\nx3 =",x,"m\ny3 =",y,"m\nz3 =",z,"m\n-----------------------")
+t = 52.5
+x = ((x2-x1)/(t2-t1))*(t - 12) + 8
+y = ((y2-y1)/(t2-t1))*(t - 12) + 6
+z = ((z2-z1)/(t2-t1))*(t - 12) + 7
+print("At time",t,"seconds:\nx4 =",x,"m\ny4 =",y,"m\nz4 =",z,"m\n-----------------------")
+t = 60.0
+x = ((x2-x1)/(t2-t1))*(t - 12) + 8
+y = ((y2-y1)/(t2-t1))*(t - 12) + 6
+z = ((z2-z1)/(t2-t1))*(t - 12) + 7
+print("At time",t,"seconds:\nx5 =",x,"m\ny5 =",y,"m\nz5 =",z,"m\n")
+# figure out the logic behind this
