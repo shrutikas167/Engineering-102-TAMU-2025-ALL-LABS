e8bbb0365c3d399b1ea3ca343159ceed40b6de60
Shrutika Suresh <shrutikas167@gmail.com>
2025-10-21 23:50:43 -0500

    2.11 LAB: Python Code Soup

---
+++ python_code_soup.py
@@ -0,0 +1,94 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: SHRUTIKA SURESH
+# Section: 406/50
+# Assignment: Lab 2 - More Linear Interpolation
+# Date: 11/10/2025
+#
+# z=1
+x = 1
+z = 0
+z += x
+print (z)
+# one done
+#
+# z=29
+y = 10
+x += 1
+y *= x
+y += x
+y += x
+y += x
+y += x
+z += y
+print (z)
+# twenty nine done
+#
+# z = 102
+y = 10
+y *= x
+y *= x
+y *= x
+z += y
+z = 0
+z += y
+y = 10
+z += y
+z+=y
+z+=x
+print (z)
+# one hundred two done
+#
+# z = 10^14
+y = 10
+x = y
+z = 0
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+z += y
+print (z)
+# ten to the power of fourteen
+#
+#8675
+x = 1
+y = 10
+z = 0
+
+x = y
+y += x
+x += 1
+z += x
+x += 1
+y *= x
+y *= x
+z += y
+y += x
+z += y
+z += y
+
+print(z)   # -> 8675
+
+
+
+
+
+y = 10
+x = y
+x += 1
+y += x
+y *= x
+z += x
+z += y
